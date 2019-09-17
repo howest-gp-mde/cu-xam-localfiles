fad62ecd9d699b01868b62f2faf1183bcbfd6f53
sigged <sderdeyn@gmail.com>
2019-09-17 21:51:26 +0200

    Save a complex object by serializing to json using a folder retrieved by Xamarin Essentials

--- XrnCourse.LocalFiles/XrnCourse.LocalFiles/CoffeeSettingsPage.xaml.cs
+++ XrnCourse.LocalFiles/XrnCourse.LocalFiles/CoffeeSettingsPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XrnCourse.LocalFiles.Domain;
@@ -11,6 +15,28 @@ namespace XrnCourse.LocalFiles
         public CoffeeSettingsPage()
         {
             InitializeComponent();
+        }
+
+        private void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            string fullPath = Path.Combine(FileSystem.AppDataDirectory, Constants.CoffeeSettingsFileName);
+            if (File.Exists(fullPath))
+            {
+                string jsonText = File.ReadAllText(fullPath);
+                try
+                {
+                    var savedSettings = JsonConvert.DeserializeObject<CoffeeSettings>(jsonText);
+                    txtCoffeeName.Text = savedSettings.CoffeeName;
+                    swHasSugar.On = savedSettings.HasSugar;
+                    sldMilkAmount.Value = savedSettings.MilkAmount;
+                    tpBrewTime.Time = savedSettings.BrewTime;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Error reading settings: {ex.Message}");
+                    //todo: log error!
+                }
+            }
         }
 
         private void BtnResetToDefaults_Clicked(object sender, EventArgs e)
@@ -31,8 +57,15 @@ namespace XrnCourse.LocalFiles
                 BrewTime = tpBrewTime.Time
             };
 
-            //todo: save settings object to JSON file!
+            //object to json string
+            string jsonText = JsonConvert.SerializeObject(settings);
 
+            //get appdata dir using Xamaring Essentials
+            string folder = FileSystem.AppDataDirectory;
+
+            //standard,simple save operation
+            string fullPath = Path.Combine(folder, Constants.CoffeeSettingsFileName);
+            File.WriteAllText(fullPath, jsonText);
         }
 
         private void SldMilkAmount_ValueChanged(object sender, ValueChangedEventArgs e)
