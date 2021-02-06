893e43ff6679279ae3b189a9b2892c1a297eea24
sigged <sderdeyn@gmail.com>
2021-02-06 14:40:44 +0100

    Revert "Read bundled pdf file from platform packages using Xamarin Essentials"
    This reverts commit ab19a2f6955f24d0c42f8f6d391863805604d949.

--- XrnCourse.LocalFiles/XrnCourse.LocalFiles/ShowBundledPdfPage.xaml.cs
+++ XrnCourse.LocalFiles/XrnCourse.LocalFiles/ShowBundledPdfPage.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,16 +12,12 @@ namespace XrnCourse.LocalFiles
             InitializeComponent();
         }
 
-        private async void BtnLoadPdf_Clicked(object sender, EventArgs e)
+        private void BtnLoadPdf_Clicked(object sender, EventArgs e)
         {
-            using (var fileStream = await FileSystem
-                .OpenAppPackageFileAsync("MyPdfs\\CSharpCheatSheet.pdf"))
-            {
-                MemoryStream memoryStream = new MemoryStream();
-                await fileStream.CopyToAsync(memoryStream);
-                pdfViewer.InputFileStream = memoryStream;
-                lblPdfSize.Text = $"{(memoryStream.Length / 1024.0):N2} kB";
-            }
+            //todo: open packaged PDF stream using Xamarin.Essentials
+
+            //todo: update filesize
+
         }
     }
 }
